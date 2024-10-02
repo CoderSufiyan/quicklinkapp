7a3f7cc4e04b751c12353a83c891b1c7b971a34f
codersufiyan <sufiyanandkhan@gmail.com>
2024-10-02 12:30:09 +0530

    changed url

--- QuickLink/server-app/dist/server.js
+++ QuickLink/server-app/dist/server.js
@@ -17,7 +17,7 @@ app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)({
     origin: [
         "http://localhost:3000", // Local development
-        "https://quicklinkapp.onrender.com", // Deployed frontend
+        "https://quicklinkapp-1.onrender.com", // Deployed frontend
     ],
     credentials: true,
 }));
